01b5f804f3a2306a5ae94b58725bafc9dedbfd36
Yuri Gabriel <yuri@aqua.com.br>
2023-01-25 11:42:57 -0300

    markup

--- src/app.js
+++ src/app.js
@@ -2,39 +2,42 @@
 
 import React, { Component } from 'react'
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      checked: false,
-      showContent: false
-    }
-  }
-  render() {
-    return (
-      <div>
-        <label>
-          <input
-            type='checkbox'
-            checked={this.state.checked}
-            onChange={() => {
-              this.setState({
-                checked: !this.state.checked,
-              }, () => {
-                this.setState({
-                  showContent: this.state.checked
-                });
-              })
-
-
-
-            }} />
-          Mostra conteúdo
-        </label>
-        {this.state.showContent && <div> Olha eu aqui</div>}
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input
+        type="search"
+        placeholder='digite o nome do usuário'
+      />
+    </div>
+    <div className='user-info'>
+      <img src='https://avatars.githubusercontent.com/u/22206310?v=4' />
+      <h1>
+        <a href='https://github.com/YuriAncelmo'>Yuri Ancelmo</a>
+      </h1>
+      <ul className='repos-info'>
+        <li> Repositórios: 15</li>
+        <li> Seguidores: 1</li>
+        <li> Seguindo: 1</li>
+      </ul>
+      <div className='actions'>
+        <button> Ver repositórios </button>
+        <button> Ver favoritos </button>
       </div>
-    )
-  }
-}
+      <div className='repos'>
+        <h2>Repositórios</h2>
+        <ul className='repos'>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+      <div className='starred'>
+        <h2> Favoritos </h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
 
 export default App
